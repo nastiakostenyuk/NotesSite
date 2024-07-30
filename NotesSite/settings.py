b435f84584761c479e938546b5691bf93ec1a4e7
anastasia_kostenyuk <nastuakostenyk@gmail.com>
2024-07-30 22:50:00 +0300

    edit settings, add env.example

--- NotesSite/settings.py
+++ NotesSite/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'NotesSite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("POSTGRES_HOST"),
+        'PORT': env("POSTGRES_PORT"),
     }
 }
 
